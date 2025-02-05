47bab5e06e5bdc4aafca257ab706cb9ed5520fdb
Artur Flisek <artur.flisek@pentacomp.pl>
2025-02-05 22:56:13 +0100

    dodanie testów jednostkowych

--- src/app/components/to-do-list-entry/to-do-list-entry.component.spec.ts
+++ src/app/components/to-do-list-entry/to-do-list-entry.component.spec.ts
@@ -1,23 +1,55 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ToDoListEntryComponent } from './to-do-list-entry.component';
+import { Store} from "@ngrx/store";
+import { Task } from '../../models/to-do-list.models';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {toDoListActions} from "../../store/to-do-list.actions";
+import {MockStore, provideMockStore} from "@ngrx/store/testing";
 
 describe('ToDoListEntryComponent', () => {
   let component: ToDoListEntryComponent;
   let fixture: ComponentFixture<ToDoListEntryComponent>;
+  let store: MockStore;
+
+  const task: Task = {name: 'Create project', completed: false};
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ToDoListEntryComponent]
+      imports: [ToDoListEntryComponent, BrowserAnimationsModule],
+      providers: [provideMockStore()]
     })
     .compileComponents();
-    
+
     fixture = TestBed.createComponent(ToDoListEntryComponent);
     component = fixture.componentInstance;
+    store = TestBed.inject(Store) as MockStore;
+
+    component.task = task;
+
     fixture.detectChanges();
+    spyOn(component, 'markComplete').and.callThrough();
+    spyOn(component, 'removeTask').and.callThrough();
+    spyOn(store, 'dispatch');
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Should remove task', () => {
+    const button:HTMLButtonElement = fixture.debugElement.nativeElement.querySelector('mat-form-field button');
+    button.click();
+
+    expect(component.removeTask).toHaveBeenCalled();
+    expect(store.dispatch).toHaveBeenCalledWith(toDoListActions.removeTask({task: task.name}));
+  })
+
+  it('Should mark completed', () => {
+    const checkbox:HTMLInputElement = fixture.debugElement.nativeElement.querySelector('mat-checkbox input');
+    checkbox.click();
+
+    expect(component.markComplete).toHaveBeenCalledWith(true);
+    expect(store.dispatch).toHaveBeenCalledWith(toDoListActions.markTaskCompleted({task: {...task, completed: true}}));
+  })
 });
--- src/app/containers/to-do-list/to-do-list.component.spec.ts
+++ src/app/containers/to-do-list/to-do-list.component.spec.ts
@@ -1,23 +1,46 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ToDoListComponent } from './to-do-list.component';
+import {MockStore, provideMockStore} from "@ngrx/store/testing";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {Store} from "@ngrx/store";
+import {toDoListActions} from "../../store/to-do-list.actions";
 
 describe('ToDoListComponent', () => {
   let component: ToDoListComponent;
   let fixture: ComponentFixture<ToDoListComponent>;
+  let store: MockStore;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ToDoListComponent]
+      imports: [ToDoListComponent, BrowserAnimationsModule],
+      providers: [provideMockStore()]
     })
     .compileComponents();
-    
+
     fixture = TestBed.createComponent(ToDoListComponent);
     component = fixture.componentInstance;
+    store = TestBed.inject(Store) as MockStore;
+
+    component.value = 'test';
+
     fixture.detectChanges();
+    spyOn(component, 'addTaskToList').and.callThrough();
+    spyOn(store, 'dispatch');
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+
+    component.ngOnInit();
+    expect(store.dispatch).toHaveBeenCalledWith(toDoListActions.fetchTasksFromCache());
+  });
+
+  it('should add task', () => {
+    const button:HTMLButtonElement = fixture.debugElement.nativeElement.querySelector('mat-card-content>button');
+    button.click();
+
+    expect(component.addTaskToList).toHaveBeenCalled();
+    expect(store.dispatch).toHaveBeenCalledWith(toDoListActions.addTask({task: 'test'}));
   });
 });
